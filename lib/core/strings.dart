03ff82f277c6b4eb509acd7f23607d17c66c6839
saju <sajusajuptl@gmail.com>
2023-03-18 10:33:50 +0530

    download section api test

---
+++ lib/core/strings.dart
@@ -0,0 +1,1 @@
+const cbaseUrl="https://api.themoviedb.org/3";
---
+++ lib/domain/download/core/api_end_point.dart
@@ -0,0 +1,6 @@
+import 'package:netflix_npp/core/strings.dart';
+import 'package:netflix_npp/infrastructor/api_key.dart';
+
+class ApiEndPoint {
+  static const downloads="$cbaseUrl/trending/all/day?api_key=$apikey";
+}
